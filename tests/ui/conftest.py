3f2de5c076abcb6157c16c8075f970bc0e2a6cf5
Daria Kudriavtseva <situation96@mail.ru>
2023-06-30 12:22:15 +0300

    Update README.md

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -15,7 +15,7 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def api_ui_client():
     client = BaseSession(base_url=os.getenv('base_url'))
     return client
@@ -24,29 +24,29 @@ def api_ui_client():
 '''TODO: When the selenoid service will work - uncomment all'''
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def browser_configuration():
-    # options = Options()
-    #
-    # selenoid_capabilities = {
-    #     "browserName": 'chrome',
-    #     "browserVersion": '100.0',
-    #     "selenoid:options": {"enableVNC": True, "enableVideo": False},
-    # }
-    #
-    # options.capabilities.update(selenoid_capabilities)
-    #
-    # s_login = os.getenv('LOGIN')
-    # s_password = os.getenv('PASSWORD')
-    #
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{s_login}:{s_password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options,
-    # )
-    # driver.implicitly_wait(50)
-    # browser.config.driver = driver
+    options = Options()
 
-    # browser.config.browser_name = "chrome"
+    selenoid_capabilities = {
+        "browserName": 'chrome',
+        "browserVersion": '100.0',
+        "selenoid:options": {"enableVNC": True, "enableVideo": False},
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    s_login = os.getenv('LOGIN')
+    s_password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{s_login}:{s_password}@selenoid.autotests.cloud/wd/hub",
+        options=options,
+    )
+    driver.implicitly_wait(50)
+    browser.config.driver = driver
+
+    browser.config.browser_name = "chrome"
     browser.config.base_url = os.getenv('base_url')
 
     yield browser
@@ -54,11 +54,11 @@ def browser_configuration():
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_video(browser)
     browser.quit()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def login_demoshop(api_ui_client):
     login = os.getenv('user_login')
     password = os.getenv('user_password')
@@ -76,7 +76,7 @@ def login_demoshop(api_ui_client):
     return api_ui_client
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def open_browser_through_api(login_demoshop, browser_configuration):
     token = login_demoshop.cookies.get('NOPCOMMERCE.AUTH')
     browser.open("")
